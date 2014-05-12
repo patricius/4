add7b7325ba755ab998d93018073effdea7c938d
patricius <gellar.p@gmail.com>
2014-05-12 10:42:55 -0700

    dddd

---
+++ 4/Scripts/app/app.js
@@ -0,0 +1,23 @@
+﻿
+var app = angular.module('FoursquareApp', ['ngRoute', 'ngResource', 'ui.bootstrap', 'toaster', 'chieffancypants.loadingBar']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/explore", {
+        controller: "placesExplorerController",
+        templateUrl: "scripts/app/views/placesresults.html"
+    });
+
+    $routeProvider.when("/places", {
+        controller: "myPlacesController",
+        templateUrl: "scripts/app/views/myplaces.html"
+    });
+
+    $routeProvider.when("/about", {
+        controller: "aboutController",
+        templateUrl: "scripts/app/views/about.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/explore" });
+
+});
