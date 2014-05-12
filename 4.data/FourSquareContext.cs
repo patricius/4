939c5118b6b1063b2eadf627f763bed115196b76
patricius <gellar.p@gmail.com>
2014-05-12 11:03:11 -0700

    data layer
    for foursquare

---
+++ 4.data/FourSquareContext.cs
@@ -0,0 +1,32 @@
+﻿using _4.data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.data
+{
+    public class FourSquareContext : DbContext
+    {
+        public FourSquareContext() :
+            base("DBConnection")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<FourSquareContext, FourSquareContextMigrationConfiguration>());
+        }
+
+        public DbSet<BookmarkedPlace> BookmarkedPlaces { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new BookmarkedPlaceMapper());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ 4.data/Migrations/FourSquareContextMigrationConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+
+namespace _4.data
+{
+    class FourSquareContextMigrationConfiguration : DbMigrationsConfiguration<FourSquareContext>
+    {
+        public FourSquareContextMigrationConfiguration()
+        {
+            this.AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = true;
+
+        }
+    }
+}
